9a8f7ab2a879b508c0f49d2d3b34dd2370a8da51
MARTI <marti@users.noreply.github.com>
2026-07-18 16:50:32 -0300

    fix: update CI to v5, add secrets for locked user, add reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   timeout: 30000,
   retries: 1,
+  reporter: [['html', { open: 'never' }]],
   use: {
     baseURL: 'https://www.saucedemo.com',
     video: 'on',
